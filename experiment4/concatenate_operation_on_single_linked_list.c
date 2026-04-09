1d64ce37aadd81e4f8cff8a0e908a3722544f02e
vedanttarare-stack <vedanttarare@gmail.com>
2026-04-09 14:34:07 +0530

    Update concatenate_operation_on_single_linked_list.c

--- experiment4/concatenate_operation_on_single_linked_list.c
+++ experiment4/concatenate_operation_on_single_linked_list.c
@@ -1,78 +1,47 @@
+/*Write a C program to implement a menu-driven application for performing operations on two Singly Linked Lists. 
+The program should allow the user to create two singly linked lists, display them, and concatenate the second list to the end of the first list.*/
+
+#include <stdio.h>
+#include <stdlib.h>
 struct node {
     int data;
     struct node* next;
 };
-
 typedef struct node* NODE;
-
 NODE createNode(int x) {
-     NODE temp = (NODE)malloc(sizeof(struct node));
-    if(temp == NULL) {
+    NODE newNode = (NODE)malloc(sizeof(struct node));
+    if(newNode == NULL) {
         printf("Memory allocation failed\n");
         exit(1);
     }
-    temp->data = x;
-    temp->next = NULL;
-    return temp;
-    
-    
-    
+    newNode->data = x;
+    newNode->next = NULL;
+    return newNode;
 }
-
 NODE addNode(NODE first, int x) {
-     NODE temp = createNode(x);
-
-    if(first == NULL) {
-        return temp;
-    }
-
-    NODE current = first;
-    while(current->next != NULL) {
-        current = current->next;
-    }
-
-    current->next = temp;
+    NODE newNode = createNode(x);
+    if(first == NULL)
+        return newNode;
+    NODE temp = first;
+    while(temp->next != NULL)
+        temp = temp->next;
+    temp->next = newNode;
     return first;
-    
-    
-    
-    
-    
-    
 }
-
 NODE concatenate(NODE t1, NODE t2) {
     if(t1 == NULL)
         return t2;
-
-    if(t2 == NULL)
-        return t1;
-
-    NODE current = t1;
-    while(current->next != NULL) {
-        current = current->next;
-    }
-
-    current->next = t2;
+    NODE temp = t1;
+    while(temp->next != NULL)
+        temp = temp->next;
+    temp->next = t2;
     return t1;
-    
-    
-    
 }
-
 void displayList(NODE first) {
-     NODE temp = first;
-
+    NODE temp = first;
     while(temp != NULL) {
-        printf("%d --> ", temp->data);
+        printf("%d -> ", temp->data);
         temp = temp->next;
     }
-
     printf("NULL\n");
-
-
-    
-    
-    
-    
 }
