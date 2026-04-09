1efa17231f15f23f3b6322ce36d22392798e9439
vedanttarare-stack <vedanttarare@gmail.com>
2026-04-09 14:29:39 +0530

    Update bubble_sort.c

--- experiment2/bubble_sort.c
+++ experiment2/bubble_sort.c
@@ -1,33 +1,25 @@
+/*Write a C program to check whether a given element is present in an array of elements using linear search.
+  The program should prompt the user to enter the size of the array, the elements of the array, and the element to search for.*/
 #include <stdio.h>
 
-void bubbleSort(int arr[], int n) {
-    
-    for (int i =0; i < n - 1; i++)
-	 for(int j = 0; j < n - i - 1; j++) {
-		 if (arr[j] > arr[j + 1]) {
-			 int temp = arr[j];
-			 arr[j] = arr[j + 1];
-			 arr[j + 1] = temp;
-			 
-		 }
-	 }
-}
-
 int main() {
-    int n;
+    int n, key, i, found = 0;
     scanf("%d", &n);
     int arr[n];
-
-    for (int i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
+    scanf("%d", &key);
 
-    bubbleSort(arr, n);
-
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-
-    return 0;
+    for(i=0;i<n;i++){
+		if(arr[i] == key){
+			found = 1;
+			break;
+		}
+	}
+	if(found==1){
+		printf("found at position %d\n", i);
+	}else{
+		printf("%d not found\n", key);
+	}
 }
